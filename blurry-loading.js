d608513f0c61250a06446939401ae0b923023509
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-28 11:19:48 -0800

    add functunality of the page

--- blurry-loading.js
+++ blurry-loading.js
@@ -1,1 +1,20 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
+let load = 0
+
+let int = setInterval(blurring, 30)
+function blurring() {
+  load++
+
+  if(load > 99) {
+  clearInterval(int)
+  }
+
+loadText.innerText = `${load}%`
+loadText.style.opacity = scale(load, 0, 100, 1, 0) // as it goes from o to 100, opacity is set from 1 to 0
+}
+
+const scale (number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
